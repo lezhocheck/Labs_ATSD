0bbd0ffc6d2973a0ea9c7fc69e34bf1b67e8f67d
_lezhocheck <oleh.toporkov@nure.ua>
2021-02-28 16:40:27 +0200

    Added new class Node, added Node constructor

--- Lab1/Lab1/LinkedList.cs
+++ Lab1/Lab1/LinkedList.cs
@@ -1,5 +1,16 @@
 ﻿using System;
 
+class Node 
+{
+    public int data;
+    public Node nextNode;
+
+    Node(int data)
+    {
+        this.data = data;
+        nextNode = null;
+    }
+}
 class LinkedList
 {
     LinkedList()
