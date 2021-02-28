06eddc527b4e4855ffd69a1b9c80eebed91d0848
_lezhocheck <oleh.toporkov@nure.ua>
2021-02-28 16:28:02 +0200

    Added empty class Linked List, added constructor and destructor

---
+++ Lab1/Lab1/LinkedList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class LinkedList
+{
+    LinkedList()
+    {
+
+    }
+    ~LinkedList()
+    {
+
+    }
+}
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 
-namespace Lab1
+class LinkedListDemo
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
     }
 }
