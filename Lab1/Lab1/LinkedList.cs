b1c6c26829edec250ee4425ced066e32311e8a39
lezhocheck <oleh.toporkov@nure.ua>
2021-03-17 21:28:46 +0200

    Implemented reverse function for Linked List. Fixed some bugs.

--- Lab1/Lab1/LinkedList.cs
+++ Lab1/Lab1/LinkedList.cs
@@ -1,155 +1,173 @@
 ﻿using System;
 
-class Node<T> 
+namespace Lab1
 {
-    public T Data { get; }
-    public Node<T> NextNode { get; set; }
+    class Node<T> 
+    {
+        public T Data { get; }
+        public Node<T> NextNode { get; set; }
 
-    public Node(T data)
-    {
-        this.Data = data;
-        NextNode = null;
-    }
-}
-class LinkedList<T> where T : IComparable
-{
-    public Node<T> Head { get; private set; }
-    public int Size { get; private set; }
-
-    public LinkedList()
-    {
-        Head = null;
-        Size = 0;
-    }
-    public LinkedList(params T[] input) 
-    {
-        Array.Sort(input);
-        Size = input.Length;
-        Node<T> temp = new Node<T>(input[0]);
-        Head = temp;
-        for (int i = 1; i < Size; i++)
+        public Node(T data)
         {
-            Node<T> tt = temp;
-            temp = new Node<T>(input[i]);
-            tt.NextNode = temp;
-        }
+            this.Data = data;
+            NextNode = null;
+        } 
     }
     
-    public void AddItem(T item)
+    class LinkedList<T> where T : IComparable
     {
-        Node<T> temp = Head;
-        Node<T> node = new Node<T>(item);
-        if(Head == null || Head.Data.CompareTo(item) > 0)
+        public Node<T> Head { get; private set; }
+        public int Size { get; private set; }
+
+        public LinkedList()
         {
-            Head = node;
-            Head.NextNode = temp;
+            Head = null;
+            Size = 0;
         }
-        else
+        public LinkedList(params T[] input) 
         {
-            while(temp.NextNode != null && temp.Data.CompareTo(item) < 0)
+            Array.Sort(input);
+            Size = input.Length;
+            Node<T> temp = new Node<T>(input[0]);
+            Head = temp;
+            for (int i = 1; i < Size; i++)
             {
-                temp = temp.NextNode;
+                Node<T> tt = temp;
+                temp = new Node<T>(input[i]);
+                tt.NextNode = temp;
             }
-            node.NextNode = temp.NextNode;
-            temp.NextNode = node;
         }
-        Size++;
-    }
-    public bool IsEmpty()
-    {
-        return Size == 0;
-    }
-    public void MakeEmpty()
-    {
-        Head = null;
-        Size = 0;
-    }
-    public bool SearchItem(T item)
-    {
-        bool result = false;
-        Node<T> temp = Head;
-        while(temp != null)
-        {
-            if(temp.Data.CompareTo(item) == 0)
-            {
-                result = true;
-                break;
-            }
-            temp = temp.NextNode;
-        }
-        return result;
-    }
-    public bool SearchItem(Node<T> node, int item)
-    {
-        if (node == null)
-            return false;
-        if (node.Data.CompareTo(item) == 0)
-            return true;
-        return SearchItem(node.NextNode, item);
-    }
-    public T GetItemByIndex(int index)
-    {
-        Node<T> temp = Head;
-        int indexer = 0;
-        if (index < 0 || index >= Size)
-        {
-            Console.WriteLine("Invalid index.");
-            return default(T);
-        }
-        while(temp != null)
-        {
-            if(indexer == index)
-            {
-                return temp.Data;
-            }
-            indexer++;
-            temp = temp.NextNode;
-        }
-        Console.WriteLine("Invalid index.");
-        return default(T);
-    }
-    public T DeleteItem(T item)
-    {
-        if (SearchItem(item))
+        
+        public void AddItem(T item)
         {
             Node<T> temp = Head;
-            Node<T> next = Head.NextNode;
-            if(temp.Data.CompareTo(item) == 0 && next != null)
+            Node<T> node = new Node<T>(item);
+            if(Head == null || Head.Data.CompareTo(item) > 0)
             {
-                Head = next;
-                Size--;
-                return item;
+                Head = node;
+                Head.NextNode = temp;
             }
-            else if(temp.Data.CompareTo(item) == 0 && next == null)
+            else
             {
-                Head = null;
-                Size = 0;
-                return item;
-            }
-            do
-            {
-                if (next.Data.CompareTo(item) == 0)
+                while(temp.NextNode != null && temp.NextNode.Data.CompareTo(item) < 0)
                 {
-                    temp.NextNode = next.NextNode;
+                    temp = temp.NextNode;
+                }
+                node.NextNode = temp.NextNode;
+                temp.NextNode = node;
+            }
+            Size++;
+        }
+        public LinkedList<T> Reverse()
+        {
+            Node<T> oldListTemp = Head;
+            LinkedList<T> newList = new LinkedList<T>();
+            while (oldListTemp != null)
+            {
+                Node<T> tempPointer = newList.Head;
+                newList.Head = new Node<T>(oldListTemp.Data);
+                newList.Head.NextNode = tempPointer;
+                oldListTemp = oldListTemp.NextNode;
+            }
+            return newList;
+        }
+        public bool IsEmpty()
+        {
+            return Size == 0;
+        }
+        public void MakeEmpty()
+        {
+            Head = null;
+            Size = 0;
+        }
+        public bool SearchItem(T item)
+        {
+            bool result = false;
+            Node<T> temp = Head;
+            while(temp != null)
+            {
+                if(temp.Data.CompareTo(item) == 0)
+                {
+                    result = true;
                     break;
                 }
                 temp = temp.NextNode;
-                next = temp.NextNode;
-            } while (next != null && temp.Data.CompareTo(item) != 0);
-            Size--;
-            return item;
+            }
+            return result;
         }
-        Console.WriteLine("List does not contain element '" + item + "'.");
-        return default;
-    }
-    public void Print()
-    {
-        Node<T> temp = Head;
-        while(temp != null)
+        public bool SearchItem(Node<T> node, int item)
         {
-            Console.Write(temp.Data + " ");
-            temp = temp.NextNode;
+            if (node == null)
+                return false;
+            if (node.Data.CompareTo(item) == 0)
+                return true;
+            return SearchItem(node.NextNode, item);
         }
-        Console.Write("\n");
-    }
+        public T GetItemByIndex(int index)
+        {
+            Node<T> temp = Head;
+            int indexer = 0;
+            if (index < 0 || index >= Size)
+            {
+                Console.WriteLine("Invalid index.");
+                return default(T);
+            }
+            while(temp != null)
+            {
+                if(indexer == index)
+                {
+                    return temp.Data;
+                }
+                indexer++;
+                temp = temp.NextNode;
+            }
+            Console.WriteLine("Invalid index.");
+            return default(T);
+        }
+        public T DeleteItem(T item)
+        {
+            if (SearchItem(item))
+            {
+                Node<T> temp = Head;
+                Node<T> next = Head.NextNode;
+                if(temp.Data.CompareTo(item) == 0 && next != null)
+                {
+                    Head = next;
+                    Size--;
+                    return item;
+                }
+                else if(temp.Data.CompareTo(item) == 0 && next == null)
+                {
+                    Head = null;
+                    Size = 0;
+                    return item;
+                }
+                do
+                {
+                    if (next.Data.CompareTo(item) == 0)
+                    {
+                        temp.NextNode = next.NextNode;
+                        break;
+                    }
+                    temp = temp.NextNode;
+                    next = temp.NextNode;
+                } while (next != null && temp.Data.CompareTo(item) != 0);
+                Size--;
+                return item;
+            }
+            Console.WriteLine("List does not contain element '" + item + "'.");
+            return default;
+        }
+        public void Print()
+        {
+            Node<T> temp = Head;
+            while(temp != null)
+            {
+                Console.Write(temp.Data + " ");
+                temp = temp.NextNode;
+            }
+            Console.Write("\n");
+        } 
+   }
 }
+
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,46 +1,46 @@
 ﻿using System;
 
-namespace  La
+namespace Lab1
 {
-    
-}
-class LinkedListDemo
-{
-    static void Main(string[] args)
-    {
-        LinkedList<int> list1 = new LinkedList<int>(1, 0, -5, 1, 0, -5, 1, 0, -5);
-        LinkedList<string> list2 = new LinkedList<string>();
-        list1.AddItem(2);
-        list1.AddItem(1);
-        list1.AddItem(0);
-        list1.AddItem(5);
-        list1.AddItem(-5);
-        list1.AddItem(0);
-        list1.AddItem(5);
-        list1.AddItem(-5);
-        list1.Print();
-        list1.DeleteItem(2);
-        list1.Print();
-        list1.DeleteItem(-5);
-        list1.Print();
-        list1.DeleteItem(100);
-        list1.Print();
-        Console.WriteLine(list1.Size);
-        Console.WriteLine(list1.SearchItem(0)); // usual
-        Console.WriteLine(list1.SearchItem(list1.Head, 1)); // recursive 
-        Console.WriteLine(list1.GetItemByIndex(2));
-        list1.MakeEmpty();
-        list1.Print();
-
-        list2.AddItem("c#");
-        list2.AddItem("c++");
-        list2.Print();
-        list2.DeleteItem("c#");
-        list2.AddItem("c++");
-        list2.Print();
-        Console.WriteLine(list2.Size);
-        list2.AddItem("python");
-        list2.Print();
-        Console.WriteLine(list2.SearchItem("python"));
-    }
+   class LinkedListDemo
+   {
+       static void Main(string[] args)
+       {
+           LinkedList<int> list1 = new LinkedList<int>(1, 0, 1, 0, -5, 0, -5, 14, 5);
+           list1.Print();
+           LinkedList<string> list2 = new LinkedList<string>();
+           LinkedList<int> list3 = list1.Reverse();
+           list3.Print();
+           list1.AddItem(0);
+           list1.AddItem(5);
+           list1.AddItem(-5);
+           list1.AddItem(0);
+           list1.AddItem(7);
+           list1.AddItem(-5);
+           list1.Print();
+           list1.DeleteItem(2);
+           list1.Print();
+           list1.DeleteItem(-5);
+           list1.Print();
+           list1.DeleteItem(100);
+           list1.Print();
+           Console.WriteLine(list1.Size);
+           Console.WriteLine(list1.SearchItem(0)); // usual
+           Console.WriteLine(list1.SearchItem(list1.Head, 1)); // recursive 
+           Console.WriteLine(list1.GetItemByIndex(2));
+           list1.MakeEmpty();
+           list1.Print();
+   
+           list2.AddItem("c#");
+           list2.AddItem("c++");
+           list2.Print();
+           list2.DeleteItem("c#");
+           list2.AddItem("c++");
+           list2.Print();
+           Console.WriteLine(list2.Size);
+           list2.AddItem("python");
+           list2.Print();
+           Console.WriteLine(list2.SearchItem("python"));
+       }
+   } 
 }
