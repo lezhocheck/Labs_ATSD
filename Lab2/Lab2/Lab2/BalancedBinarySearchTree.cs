aacd8bb85a5371dab71f7f0e769e16aba23f89d9
lezhocheck <oleh.toporkov@nure.ua>
2021-03-27 18:32:58 +0200

    Added PrintSorted, CountNode and SumKeys functions to the BBST class.

--- Lab2/Lab2/Lab2/BalancedBinarySearchTree.cs
+++ Lab2/Lab2/Lab2/BalancedBinarySearchTree.cs
@@ -27,6 +27,51 @@ namespace Lab2
             Console.WriteLine("Item was not deleted.");
         }
 
+        public void PrintSorted()
+        {
+            List<Node> list = new List<Node>();
+            ToListRec(list, Root);
+
+            foreach (var node in list)
+            {
+                Console.Write($"{node.Data} ");
+            }
+            Console.WriteLine();
+            
+            list.Reverse();
+            
+            foreach (var node in list)
+            {
+                Console.Write($"{node.Data} ");
+            }
+            Console.WriteLine();
+        }
+
+        public int CountNode()
+        {
+            return CountNodeRec(Root);
+        }
+
+        public int SumKeys()
+        {
+            return SumKeysRec(Root);
+        }
+        
+        private int CountNodeRec(Node r)
+        {
+            if (r.LNode == null)
+                return 0;
+
+            return CountNodeRec(r.LNode) + 1;
+        }
+
+        private int SumKeysRec(Node r)
+        {
+            if (r.RNode == null)
+                return 0;
+            return SumKeysRec(r.RNode) + r.RNode.Data;
+        }
+        
         private void Rebalance()
         {
             List<Node> list = new List<Node>();
--- Lab2/Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Lab2/Program.cs
@@ -7,25 +7,21 @@ namespace Lab2
         static void Main(string[] args)
         {
             BalancedBinarySearchTree bbst = new BalancedBinarySearchTree();
-            BinarySearchTree bst = new BinarySearchTree();
-            bst.AddItem(1);
-            bst.AddItem(2);
-            bst.AddItem(3);
-            bst.AddItem(4);
-            bst.AddItem(5);
-            bst.AddItem(6);
-            bst.AddItem(7);
-
-            bbst.AddItem(1);
-            bbst.AddItem(2);
+            
             bbst.AddItem(3);
-            bbst.AddItem(4);
-            bbst.AddItem(5);
-            bbst.AddItem(6);
+            bbst.AddItem(2);
             bbst.AddItem(7);
-
-            bst.PrintPreorder();
+            bbst.AddItem(2);
+            bbst.AddItem(8);
+            bbst.AddItem(10);
+            bbst.AddItem(-1);
+            
             bbst.PrintPreorder();
+            
+            bbst.PrintSorted();
+            
+            Console.WriteLine(bbst.CountNode());
+            Console.WriteLine(bbst.SumKeys());
         }
     }
 }
