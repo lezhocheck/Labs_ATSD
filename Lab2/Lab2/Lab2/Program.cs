08017f2bf189c08c1f5823fde43874a084e0f972
lezhocheck <oleh.toporkov@nure.ua>
2021-03-29 11:04:21 +0300

    Added some test cases.

--- Lab2/Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Lab2/Program.cs
@@ -9,14 +9,33 @@ namespace Lab2
             BalancedBinarySearchTree bbst = new BalancedBinarySearchTree();
             
             bbst.AddItem(2);
+            bbst.AddItem(2);
+            bbst.AddItem(2);
             bbst.AddItem(3);
             bbst.AddItem(1);
             bbst.AddItem(8);
             bbst.AddItem(6);
             bbst.AddItem(7);
+            bbst.AddItem(8);
             bbst.AddItem(4);
+            bbst.AddItem(8);
             bbst.AddItem(5);
-            Console.WriteLine(bbst.GetCommonAncestor(5, 2));
+            bbst.PrintPreorder();
+            
+            bbst.DeleteItem(1);
+            bbst.PrintPreorder();
+
+            bbst.DeleteDuplicate();
+            Console.WriteLine(bbst.GetCommonAncestor(5, 6));
+            bbst.PrintPreorder();
+            
+            bbst.DeleteEven();
+            bbst.PrintPreorder();
+            
+            Console.WriteLine(bbst.CountNode());
+            Console.WriteLine(bbst.FindMiddle());
+            Console.WriteLine(bbst.SumKeys());
+            Console.WriteLine(bbst.IsBalanced());
             bbst.PrintPreorder();
         }
     }
