2b1f3fae20810650e01e999005f700ed30bae20d
lezhocheck <oleh.toporkov@nure.ua>
2021-04-18 15:25:18 +0300

    Added ArrayList class with constructor and AddItem function.

---
+++ Lab3/Lab3/ArrayList.cs
@@ -0,0 +1,32 @@
+﻿namespace Lab3
+{
+    public class ArrayList
+    {
+        private int[] _array;
+        private int _last;
+
+        public ArrayList(int cap = 25)
+        {
+            _array = new int[cap];
+            _last = -1;
+        }
+
+        public void AddItem(int item)
+        {
+            if (_last + 1 >= _array.Length)
+            {
+                int[] newArray = new int[_array.Length + 10];
+                
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    newArray[i] = _array[i];
+                }
+
+                _array = newArray;
+            }
+            
+            _array[_last + 1] = item;
+            _last++;
+        }
+    }
+}
