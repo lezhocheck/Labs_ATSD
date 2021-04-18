3495d9d19da000ef63ac719c3686ccaf433b3fcb
lezhocheck <oleh.toporkov@nure.ua>
2021-04-18 15:12:43 +0300

    Created Empty project.

---
+++ Lab2/Tut1_ATSD/Tut1_ATSD/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tut1_ATSD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>(15);
+            list.AddItem(4);
+            list.AddItem(2);
+            list.AddItem(7);
+            list.AddItem(7);
+            list.AddItem(4);
+            list.AddItem(1);
+            list.AddItem(1);
+            LinkedList<int> list1 = new LinkedList<int>(15);
+            list1.AddItem(7);
+            list1.AddItem(8);
+            list1.AddItem(0);
+            list1.AddItem(5);
+            list1.AddItem(4);
+            list1.AddItem(1);
+            list1.AddItem(1);
+            list.Union(list1);
+            list.Print();
+        }
+    }
+}
