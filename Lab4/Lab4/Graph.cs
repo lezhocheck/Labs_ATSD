067a1f476120388cf96b1bf9a042692b87bf340a
lezhocheck <oleh.toporkov@nure.ua>
2021-05-10 13:47:31 +0300

    Added print function, Add Edge function

--- Lab4/Lab4/Graph.cs
+++ Lab4/Lab4/Graph.cs
@@ -4,19 +4,41 @@ namespace Lab4
 {
     public class Graph
     {
-        private int _nodesCouns; 
+        private int _size;
         private int[,] _matrix;
 
-        public Graph(int[,] matrix)
+        public Graph(int size)
         {
-            int height = matrix.GetLength(0);
-            int width = matrix.GetLength(0);
+            _size = size;
+            _matrix = new int[size, size];
+        }
 
-            if (height != width)
-                throw new ArgumentException("Width height mismatch");
+        public void AddEdge(int v1, int v2, int value)
+        {
+            if (v1 < 0 || v1 > _size - 1 || v2 < 0 || v2 > _size - 1)
+                throw new ArgumentOutOfRangeException();
 
-            _nodesCouns = width;
-            _matrix = matrix;
+            if (value == 0)
+                throw new ArgumentNullException();
+            
+            _matrix[v1, v2] = value;
+        }
+        
+        
+
+        public void Print()
+        {
+            Console.WriteLine("Graph:");
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    if (_matrix[i, j] != 0)
+                    {
+                        Console.WriteLine($"Edge '{i} {j}': {_matrix[i, j]}");
+                    }
+                }
+            }
         }
     }
 }
--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -6,7 +6,18 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter graph nodes count:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            Graph graph = new Graph(size);
+            graph.AddEdge(0, 2, 5);
+            graph.AddEdge(0, 4, 3);
+            graph.AddEdge(1, 0, 9);
+            graph.AddEdge(2, 1, 15);
+            graph.AddEdge(3, 2, 92);
+            graph.AddEdge(3, 1, 4);
+            graph.AddEdge(4, 3, 8);
+            graph.Print();
         }
     }
 }
