cb96ed83c973f99de940e3a52e69c89ce1aecf6d
lezhocheck <oleh.toporkov@nure.ua>
2021-05-10 11:57:02 +0300

    Created empty Lab 4 project

---
+++ Tut2/Tut2/BST.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tut2
+{
+    public class Node
+    {
+        public Node RNode { get; set; }
+        public Node LNode { get; set; }
+        public int Data;
+
+        public Node()
+        {
+            Data = Int32.MinValue;
+            RNode = null;
+            LNode = null;
+        }
+    }
+    
+    public class Bst
+    {
+        private Node _root;
+
+        public Bst()
+        {
+            _root = null;
+        }
+
+
+        public void MakeEmpty() => _root = null;
+
+        public void Insert(int item) =>  _root = InsertRec(item, _root);
+
+        public bool Search(int item) => SearchRec(item, _root);
+
+        private bool SearchRec(int item, Node r)
+        {
+            if (r == null)
+                return false; 
+            
+            if (r.Data == item)
+                return true;
+
+            if (item < r.Data)
+                return SearchRec(item, r.LNode);
+            else
+                return SearchRec(item, r.RNode);
+        }
+
+        public void DeleteItem(int item)
+        {
+            if (Search(item))
+            {
+                _root = DeleteItemRec(item, _root);
+                return;
+            }
+
+            throw new ArgumentException("Item was not deleted.");
+        }
+        
+        private Node DeleteItemRec(int item, Node r)
+        {
+            if (item < r.Data)
+                r.LNode = DeleteItemRec(item, r.LNode);
+            else if (item > r.Data)
+                r.RNode = DeleteItemRec(item, r.RNode);
+            else
+                r = DeleteFound(r);
+            return r;  
+        }
+
+        private Node DeleteFound(Node t)
+        {
+            if (t.LNode == null)
+                return t.RNode;
+            else if (t.RNode == null)
+                return t.LNode;
+            else
+            {
+                Node temp = t.RNode;
+                while (temp.LNode != null)
+                {
+                    temp = temp.LNode;
+                }
+
+                t.Data = temp.Data;
+                t.RNode = DeleteItemRec(t.Data, t.RNode);
+                return t;
+            }
+        }
+        private Node InsertRec(int item, Node r)
+        {
+            if (r == null)
+            {
+                r = new Node { Data = item };
+                return r;
+            }
+
+            if (item < r.Data)
+                r.LNode = InsertRec(item, r.LNode);
+            else
+                r.RNode = InsertRec(item, r.RNode);
+            return r;
+        }
+
+
+        public int Size() => SizeRec(_root);
+
+        private int SizeRec(Node r)
+        {
+            if (r == null)
+                return 0;
+            
+            return SizeRec(r.LNode) + SizeRec(r.RNode) + 1;
+        }
+
+        public int Sum() => SumRec(_root);
+
+        private int SumRec(Node root)
+        {
+            if (root == null)
+                return 0;
+
+            return root.Data + SumRec(root.LNode) + SumRec(root.RNode);
+        }
+
+        public void PrintAscending()
+        {
+            List<int> list = new List<int>();
+            ToListRec(list, _root);
+
+            foreach (var i in list)
+            {
+                Console.Write($"{i} ");
+            }
+            
+            Console.WriteLine();
+        }
+        
+        public void PrintDescending()
+        {
+            List<int> list = new List<int>();
+            ToListRec(list, _root);
+            list.Reverse();
+
+            foreach (var i in list)
+            {
+                Console.Write($"{i} ");
+            }
+            
+            Console.WriteLine();
+        }
+
+        private void ToListRec(List<int> list, Node r)
+        {
+            if(r == null)
+                return;
+            
+            ToListRec(list, r.LNode);
+            list.Add(r.Data);
+            ToListRec(list, r.RNode);
+        }
+
+        public bool IsEquals(Bst tree)
+        {
+            IsEqualsRec(this._root, tree._root, out bool res);
+            return res;
+        }
+
+        private void IsEqualsRec(Node r1, Node r2, out bool val)
+        {
+            if (r1 == null && r2 == null)
+            {
+                val = true;
+                return;
+            }
+            
+            if (r1.Data != r2.Data)
+            {
+                val = false;
+                return;   
+            }
+
+            IsEqualsRec(r1.LNode, r2.LNode, out val);
+            IsEqualsRec(r1.RNode, r2.RNode, out val);
+        }
+
+        public bool IsBalanced() => IsBalancedRec(_root);
+
+        private bool IsBalancedRec(Node r)
+        {
+            if (r == null)
+                return true;
+            
+            if (Math.Abs(GetHeight(r.LNode) - GetHeight(r.RNode)) <= 1
+                && IsBalancedRec(r.LNode) && IsBalancedRec(r.RNode))
+                return true;
+            
+            return false;
+        }
+        
+        private int GetHeight(Node r)
+        {
+            if (r == null)
+                return 0;
+
+            return Math.Max(GetHeight(r.LNode), GetHeight(r.RNode)) + 1;
+        }
+        
+        public bool SameData(Bst bst)
+        {
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+            ToListRec(list1, _root);
+            ToListRec(list2, bst._root);
+
+            if (list1.Count != list2.Count)
+                return false;
+
+            foreach (int i in list1)
+            {
+                if (!list2.Contains(i))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tut2/Tut2/LinkedList.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace Tut2
+{
+    class NodeList 
+    {
+        public int Data { get; }
+        public NodeList NextNode { get; set; }
+
+        public NodeList(int data)
+        {
+            Data = data;
+            NextNode = null;
+        } 
+    }
+    
+    class LinkedList
+    {
+        private NodeList _head;
+        public int Size { get; private set; }
+
+        public LinkedList()
+        {
+            _head = null;
+            Size = 0;
+        }
+     
+        public void AddItem(int item)
+        {
+            NodeList temp = _head;
+            NodeList node = new NodeList(item);
+            if(_head == null || _head.Data > item)
+            {
+                _head = node;
+                _head.NextNode = temp;
+            }
+            else
+            {
+                while(temp.NextNode != null && temp.NextNode.Data < item)
+                {
+                    temp = temp.NextNode;
+                }
+                node.NextNode = temp.NextNode;
+                temp.NextNode = node;
+            }
+            Size++;
+        }
+
+        public bool SearchItem(int item)
+        {
+            bool result = false;
+            NodeList temp = _head;
+            while(temp != null)
+            {
+                if(temp.Data == item)
+                {
+                    result = true;
+                    break;
+                }
+                temp = temp.NextNode;
+            }
+            return result;
+        }
+        
+        public int DeleteItem(int item)
+        {
+            if (SearchItem(item))
+            {
+                NodeList temp = _head;
+                NodeList next = _head.NextNode;
+                if(temp.Data == item && next != null)
+                {
+                    _head = next;
+                    Size--;
+                    return item;
+                }
+                else if(temp.Data == item && next == null)
+                {
+                    _head = null;
+                    Size = 0;
+                    return item;
+                }
+                do
+                {
+                    if (next.Data == item)
+                    {
+                        temp.NextNode = next.NextNode;
+                        break;
+                    }
+                    temp = temp.NextNode;
+                    next = temp.NextNode;
+                } while (next != null && temp.Data != item);
+                Size--;
+                return item;
+            }
+            
+            throw new ArgumentOutOfRangeException("List does not contain element '" + item + "'.");
+        }
+
+
+        public int Sum() => SumRec(_head);
+        private int SumRec(NodeList root)
+        {
+            if (root == null)
+                return 0;
+
+            return SumRec(root.NextNode) + root.Data;
+        }
+
+        public void Print() => PrintRec(_head);
+        public void PrindRev() => PrintRevRec(_head);
+        
+        private void PrintRec(NodeList root)
+        {
+            if (root == null)
+            {
+                Console.WriteLine();
+                return;
+            }
+
+            Console.Write($"{root.Data} ");
+            
+            PrintRec(root.NextNode);
+        }
+        
+        private void PrintRevRec(NodeList root)
+        {
+            if (root == null)
+            {
+                Console.WriteLine();
+                return;
+            }
+
+            PrintRevRec(root.NextNode);
+            
+            Console.Write($"{root.Data} ");
+        }
+        
+   }
+}
+
