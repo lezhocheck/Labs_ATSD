bf0818b058815a8b708639ac00f3e2f9038314ae
lezhocheck <oleh.toporkov@nure.ua>
2021-05-10 12:12:41 +0300

    Added graph constructor.

--- Lab4/Lab4/Graph.cs
+++ Lab4/Lab4/Graph.cs
@@ -1,7 +1,22 @@
-﻿namespace Lab4
+﻿using System;
+
+namespace Lab4
 {
     public class Graph
     {
-        
+        private int _nodesCouns; 
+        private int[,] _matrix;
+
+        public Graph(int[,] matrix)
+        {
+            int height = matrix.GetLength(0);
+            int width = matrix.GetLength(0);
+
+            if (height != width)
+                throw new ArgumentException("Width height mismatch");
+
+            _nodesCouns = width;
+            _matrix = matrix;
+        }
     }
 }
