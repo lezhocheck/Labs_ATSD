3afa6a9ad25e8e66aa5929292dd69f96c7619894
lezhocheck <oleh.toporkov@nure.ua>
2021-05-19 16:00:23 +0300

    Added Add function and DoubleCapacity function.

--- Lab5/Lab5/UnsortedArrayList.cs
+++ Lab5/Lab5/UnsortedArrayList.cs
@@ -4,20 +4,43 @@
     {
         private const int InitCapacity = 10;
         private int[] _array;
-        private int _size;
-
-        public int Count => _size;
+        
+        public int Count { get; private set; }
 
         public UnsortedArrayList()
         {
             _array = new int[InitCapacity];
-            _size = 0;
+            Count = 0;
         }
 
         public UnsortedArrayList(int capacity)
         {
             _array = new int[capacity];
-            _size = 0;
+            Count = 0;
+        }
+
+        public void Add(int value)
+        {
+            if (Count == _array.Length - 1)
+            {
+                DoubleCapacity();
+            }
+            
+            _array[Count - 1] = value;
+            Count++;
+        }
+
+        private void DoubleCapacity()
+        {
+            int cap = _array.Length;
+            int[] newArray = new int[cap * 2];
+
+            for(int i = 0; i < Count; i++)
+            {
+                newArray[i] = _array[i];
+            }
+
+            _array = newArray;
         }
     }
 }
