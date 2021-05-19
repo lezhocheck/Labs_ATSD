61997df1e9a2cd462236e0fd7983cbe7994d9c36
lezhocheck <oleh.toporkov@nure.ua>
2021-05-19 15:51:53 +0300

    Added UnsortedArrayList class with constructors.

---
+++ Lab5/Lab5/UnsortedArrayList.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab5
+{
+    public class UnsortedArrayList
+    {
+        private const int InitCapacity = 10;
+        private int[] _array;
+        private int _size;
+
+        public int Count => _size;
+
+        public UnsortedArrayList()
+        {
+            _array = new int[InitCapacity];
+            _size = 0;
+        }
+
+        public UnsortedArrayList(int capacity)
+        {
+            _array = new int[capacity];
+            _size = 0;
+        }
+    }
+}
